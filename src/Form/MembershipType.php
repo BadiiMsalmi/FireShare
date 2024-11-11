f87e4f26a0f3373cacd4618222ad7100831be6ef
khalil jabri <jabrikhalil869@gmail.com>
2024-11-11 11:03:26 +0100

    add crud of all the entities

---
+++ src/Form/MembershipType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use App\Entity\Membership;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MembershipType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('joinDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('group', EntityType::class, [
+                'class' => Group::class,
+                'choice_label' => 'id',
+            ])
+            ->add('membershipUser', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Membership::class,
+        ]);
+    }
+}
